a8ec57864a7f257652841f092a8d91e5a682f189
PC1994 <ChanHoMan@ChanHodeMacBook-Pro.local>
2015-05-16 15:35:14 +0800

    create a start scene but not finish

---
+++ Stupid/Stupid/Assets/Scritps/ExitButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ExitButton : MonoBehaviour {
+
+	public Button exitButtonObj;
+
+	// Use this for initialization
+	void Start () {
+		exitButtonObj.onClick.AddListener (exitFnc);
+	}
+	
+	void exitFnc(){
+		print ("Application quit.");
+		Application.Quit();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Stupid/Stupid/Assets/Scritps/StartGameButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StartGameButton : MonoBehaviour {
+
+	public Button startButtonObj;
+
+	// Use this for initialization
+	void Start () {
+		startButtonObj.onClick.AddListener (gotoStageFnc);
+	}
+	
+	void gotoStageFnc(){
+		Application.LoadLevel("level01");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+}
