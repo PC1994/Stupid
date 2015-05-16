4e559cfe52025376c8c377443c0e10ce5acb6ccc
PC1994 <ChanHoMan@ChanHodeMacBook-Pro.local>
2015-05-16 22:03:24 +0800

    update door script
    It will display a button which click to enter the door if player stay
    the door.

--- Stupid/Stupid/Assets/Scritps/LevelAdvance.cs
+++ Stupid/Stupid/Assets/Scritps/LevelAdvance.cs
@@ -4,13 +4,37 @@ using System.Collections;
 public class LevelAdvance : MonoBehaviour {
 
 	public string level;
+	private bool stayDoor = false;
 
-	void OnTriggerStay2D(Collider2D other){
-		if (other.tag == "Player") {
-			if(Input.GetKeyUp("up")){
-				Application.LoadLevel(level);
+	void enterDoorFnc(){
+		Application.LoadLevel (level);
+	}
+
+	void OnGUI() {
+		if (stayDoor) {
+			if (GUI.Button (new Rect (500, 10, 150, 100), "Enter door")) {
+				enterDoorFnc ();
 			}
 		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Player") {
+			stayDoor = true;
+		}
+	}
+	void OnTriggerExit2D(Collider2D other){
+		if (other.tag == "Player") {
+			stayDoor = false;
+		}
+	}
+	void OnTriggerStay2D(Collider2D other){
+
+		if (other.tag == "Player") {
+			if (Input.GetKeyUp ("up")) {
+				enterDoorFnc();
+			}
+		} 
 	}
 
 }
